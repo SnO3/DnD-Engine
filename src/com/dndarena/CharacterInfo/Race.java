7368eab290836bc6fca8e94775318d26ac3a1666
Josiah Nieves <jtnov20@hotmail.com>
2015-04-02 10:45:34 -0400

    revert comment test

--- src/com/dndarena/CharacterInfo/Race.java
+++ src/com/dndarena/CharacterInfo/Race.java
@@ -4,7 +4,7 @@ import com.dndarena.CharacterInfo.Elf.Subrace;
 
 public interface Race {
 	public static final int SIZE = 2;
-	//test
+	
 	public int getSize();
 	public int getBaseSpeed();
 	public int getBaseSpeed(Subrace s);
